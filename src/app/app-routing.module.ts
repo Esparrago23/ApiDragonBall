70c97341f8212ba444ee3e37b38f596af5f5763c
Christian <minisirhc@gmail.com>
2024-12-05 17:43:54 -0600

    [add] personajes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { PersonajesCardsComponent } from './personajes/personajes-cards/personajes-cards.component';
+const routes: Routes = [
+  {path: '', redirectTo:'/Personajes', pathMatch:'full'},
+  {path: 'Personajes', component:PersonajesCardsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/personajes/personajes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonajesCardsComponent } from './personajes-cards/personajes-cards.component';
+
+
+
+@NgModule({
+  declarations: [
+    PersonajesCardsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonajesModule { }
