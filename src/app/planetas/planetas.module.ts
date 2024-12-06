d9f1de485642f4a29c4f4863d2278c37a4adb0d4
Rodrigo Campuzano <camcluod@gmail.com>
2024-12-05 18:21:38 -0600

    Creacion Modulo y Contenido de PLanetas

---
+++ src/app/planetas/planetas.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlanetasComponent } from './planetas/planetas/planetas.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlanetasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlanetasModule { }
